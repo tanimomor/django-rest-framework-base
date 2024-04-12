102f3df86b5b6def4ef4acfd9b5739553d680f4e
Omor Al Tanim <omoraltanim@gmail.com>
2024-04-13 04:02:11 +0600

    Added entrypoint, HyperlinkedModelSerializer and pagination.

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,10 @@ from rest_framework import routers
 
 from quickstart.views import UserViewSet, GroupViewSet
 
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     # path('', include(router.urls)),
     path('', include('snippets.urls')),
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,43 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+# class SnippetSerializer(serializers.ModelSerializer):
+#     """
+#     Now owners will be shown in list but not in form.
+#     We could have also used CharField(read_only=True) here.
+#     """
+#     owner = serializers.ReadOnlyField(source='owner.username')
+#     class Meta:
+#         model = Snippet
+#         fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Now owners will be shown in list but not in form.
+    We could have also used CharField(read_only=True) here.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+#
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'snippets']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
